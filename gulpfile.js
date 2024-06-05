44865d5f2bff36e3a4ab739a814176ebee30d1bf
bleachhhhh <pavelpermyakov955@gmail.com>
2024-06-05 20:41:08 +0300

    webp dotn work

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp';
+import {path} from './gulp/config/path.js';
+import { plugins } from './gulp/config/plugins.js';
+global.app={
+    path:path,
+    gulp:gulp,
+    plugins:plugins
+}
+//Импорт задач
+import {copy} from './gulp/tasks/copy.js';
+import {del} from './gulp/tasks/reset.js';
+import {html} from './gulp/tasks/html.js';
+import {server} from './gulp/tasks/server.js'
+import {scss} from './gulp/tasks/scss.js'
+import {js} from './gulp/tasks/js.js'
+import {images} from './gulp/tasks/images.js'
+
+
+
+function watcher(){
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
+}
+const mainTasks=gulp.parallel(copy, html,scss,js);
+const dev=gulp.series(del, mainTasks, gulp.parallel(server,watcher));
+
+gulp.task('default', dev);
